d9a0d76f37db6559713065144fc990ef269773a7
Kaue Jacyntho <kauekaj@gmail.com>
2024-02-22 20:02:06 -0300

    Initial Commit

---
+++ FitPlus/FitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPlusApp.swift
+//  FitPlus
+//
+//  Created by Kaue de Assis Jacyntho on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
