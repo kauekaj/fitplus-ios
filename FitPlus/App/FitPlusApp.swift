dee8f4b4b2bc603bb182798dd32ec62f295d6c77
Kaue Jacyntho <kauekaj@gmail.com>
2024-07-23 22:32:32 -0300

    [Feature] Add firebase-ios-sdk dependency into the project

--- FitPlus/App/FitPlusApp.swift
+++ FitPlus/App/FitPlusApp.swift
@@ -5,13 +5,34 @@
 //  Created by Kaue de Assis Jacyntho on 22/02/24.
 //
 
+import Firebase 
 import SwiftUI
+
 
 @main
 struct FitPlusApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             LoginView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
     }
 }
