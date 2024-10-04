5dd33fb305fe91af7ac5e24b109472ac82071ec0
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-03 22:24:51 -0300

    Improvements into RecipeListView

--- FitPlus/Core/Recipes/RecipesView.swift
+++ FitPlus/Core/Recipes/RecipesView.swift
@@ -13,6 +13,7 @@ struct RecipeModel: Identifiable, Codable {
     let name: String
     let imageUrl: String
     let ingredients: [String]
+    let instructions: String
 }
 
 final class RecipeListViewModel: ObservableObject {
@@ -22,131 +23,152 @@ final class RecipeListViewModel: ObservableObject {
     func fetchRecipesFromJson() {
         let mockJson = """
         {
-            "sweet_recipes": [
-                {
-                    "id": "1",
-                    "name": "Bolo de Chocolate",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Farinha", "Chocolate", "Açúcar", "Ovos", "Manteiga"]
-                },
-                {
-                    "id": "2",
-                    "name": "Torta de Limão",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Limão", "Leite Condensado", "Biscoito", "Creme de Leite"]
-                },
-                {
-                    "id": "3",
-                    "name": "Brigadeiro",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Leite Condensado", "Chocolate em Pó", "Manteiga"]
-                },
-                {
-                    "id": "4",
-                    "name": "Mousse de Maracujá",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Maracujá", "Leite Condensado", "Creme de Leite"]
-                },
-                {
-                    "id": "5",
-                    "name": "Pudim de Leite",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Leite Condensado", "Ovos", "Leite"]
-                },
-                {
-                    "id": "6",
-                    "name": "Beijinho",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Leite Condensado", "Coco Ralado", "Manteiga"]
-                },
-                {
-                    "id": "7",
-                    "name": "Cocada",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Coco", "Açúcar", "Leite"]
-                },
-                {
-                    "id": "8",
-                    "name": "Rabanada",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Pão", "Leite", "Ovos", "Açúcar"]
-                },
-                {
-                    "id": "9",
-                    "name": "Bolo de Cenoura",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Farinha", "Cenoura", "Açúcar", "Ovos"]
-                },
-                {
-                    "id": "10",
-                    "name": "Torta de Chocolate",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Chocolate", "Biscoito", "Manteiga", "Açúcar"]
-                }
-            ],
-            "savory_recipes": [
-                {
-                    "id": "1",
-                    "name": "Lasanha",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa", "Carne Moída", "Queijo", "Molho de Tomate"]
-                },
-                {
-                    "id": "2",
-                    "name": "Pizza",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa de Pizza", "Molho de Tomate", "Queijo", "Orégano"]
-                },
-                {
-                    "id": "3",
-                    "name": "Coxinha",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Frango Desfiado", "Massa de Coxinha", "Requeijão"]
-                },
-                {
-                    "id": "4",
-                    "name": "Bolinho de Bacalhau",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Bacalhau", "Batata", "Cebola", "Salsa"]
-                },
-                {
-                    "id": "5",
-                    "name": "Empada",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa", "Frango", "Catupiry"]
-                },
-                {
-                    "id": "6",
-                    "name": "Pastel",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa de Pastel", "Carne Moída", "Queijo"]
-                },
-                {
-                    "id": "7",
-                    "name": "Quiche",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa", "Queijo", "Bacon", "Ovos"]
-                },
-                {
-                    "id": "8",
-                    "name": "Esfiha",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Massa", "Carne Moída", "Cebola", "Tomate"]
-                },
-                {
-                    "id": "9",
-                    "name": "Torta Salgada",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Farinha", "Frango", "Cenoura", "Ervilha"]
-                },
-                {
-                    "id": "10",
-                    "name": "Feijoada",
-                    "image_url": "https://via.placeholder.com/150",
-                    "ingredients": ["Feijão Preto", "Carne de Porco", "Linguiça", "Farinha"]
-                }
-            ]
+          "sweet_recipes": [
+            {
+              "id": "1",
+              "name": "Bolo de Aveia e Banana",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fbolo_aveia_banana.avif?alt=media&token=f779a3dd-2bed-4080-a3ca-493d7c027d92",
+              "ingredients": ["Aveia", "Banana", "Ovos", "Mel", "Canela"],
+              "instructions": "Pré-aqueça o forno a 180°C. Bata as bananas com os ovos, adicione aveia e mel. Misture tudo e leve ao forno por 30 minutos."
+            },
+            {
+              "id": "2",
+              "name": "Torta Integral de Maçã",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Ftorta_integral_de_maca.jpg?alt=media&token=6f0257a0-91f6-48e7-a493-955041cc44ec",
+              "ingredients": ["Farinha Integral", "Maçã", "Mel", "Canela", "Ovos"],
+              "instructions": "Misture a farinha com os ovos e o mel. Coloque as maçãs em fatias sobre a massa e asse por 25 minutos a 180°C."
+            },
+            {
+              "id": "3",
+              "name": "Brigadeiro Fit",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fbrigadeiro_fit.png?alt=media&token=9b436f88-e9b4-4cd2-ac5c-58f403bd21b5",
+              "ingredients": ["Leite de Coco", "Cacau em Pó", "Adoçante Natural"],
+              "instructions": "Misture o leite de coco com cacau e adoçante. Leve ao fogo até engrossar e deixe esfriar antes de enrolar."
+            },
+            {
+              "id": "4",
+              "name": "Mousse de Abacate",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fmousse_abacate.webp?alt=media&token=093a6851-07cc-4473-8636-f45032c6e5cd",
+              "ingredients": ["Abacate", "Cacau em Pó", "Mel", "Leite de Coco"],
+              "instructions": "Bata o abacate com leite de coco, cacau e mel até obter uma consistência cremosa. Refrigere antes de servir."
+            },
+            {
+              "id": "5",
+              "name": "Pudim de Chia",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fpudim_chia.webp?alt=media&token=52f391b8-3ef9-4d51-87a2-b90bd62d7404",
+              "ingredients": ["Sementes de Chia", "Leite de Amêndoas", "Adoçante Natural", "Frutas Vermelhas"],
+              "instructions": "Misture chia com leite de amêndoas e adoçante. Deixe na geladeira por 2 horas. Sirva com frutas vermelhas."
+            },
+            {
+              "id": "6",
+              "name": "Beijinho Fit",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fbeijinho_fit.jpg?alt=media&token=3776302a-17cf-40b6-9b6e-dd13a3d5ed89",
+              "ingredients": ["Leite de Coco", "Coco Ralado", "Adoçante Natural"],
+              "instructions": "Misture o leite de coco com coco ralado e adoçante. Cozinhe até engrossar. Modele os beijinhos e sirva."
+            },
+            {
+              "id": "7",
+              "name": "Cocada de Coco com Whey",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fcocada_com_whey.jpg?alt=media&token=bd8fe2fa-0173-436a-9f86-b340d21e2b63",
+              "ingredients": ["Coco Ralado", "Whey Protein", "Leite de Coco", "Adoçante Natural"],
+              "instructions": "Misture coco ralado, whey e leite de coco. Cozinhe até engrossar e forme bolinhas ou barras."
+            },
+            {
+              "id": "8",
+              "name": "Bolo de Cenoura Fit",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fbolo_de_cenoura.jpg?alt=media&token=416f6fd6-9731-4e72-8960-fa100d46df14",
+              "ingredients": ["Cenoura", "Farinha de Amêndoas", "Ovos", "Azeite", "Mel"],
+              "instructions": "Bata a cenoura com os ovos e azeite. Adicione a farinha de amêndoas e o mel. Asse a 180°C por 35 minutos."
+            },
+            {
+              "id": "9",
+              "name": "Brownie de Batata Doce",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2Fbrownie_proteico_batata_doce.jpg?alt=media&token=9acd6daf-e21c-4959-9985-4a77263a8151",
+              "ingredients": ["Batata Doce", "Cacau em Pó", "Ovos", "Farinha de Amêndoas", "Adoçante Natural"],
+              "instructions": "Misture batata doce cozida com cacau, ovos e farinha de amêndoas. Asse por 25 minutos a 180°C."
+            },
+            {
+              "id": "10",
+              "name": "Panqueca de Banana e Aveia",
+              "image_url": "https://firebasestorage.googleapis.com/v0/b/fitplus-53f36.appspot.com/o/Recipes%2Fthumbnail%2FPanqueca_de_banana_aveia.jpeg?alt=media&token=43045dd5-7355-472f-a63e-f08c8ae63280",
+              "ingredients": ["Banana", "Aveia", "Ovos", "Canela"],
+              "instructions": "Amasse a banana e misture com ovos e aveia. Frite pequenas porções em uma frigideira antiaderente até dourar."
+            }
+          ],
+          "savory_recipes": [
+            {
+              "id": "1",
+              "name": "Lasanha de Abobrinha",
+              "image_url": "https://www.example.com/images/fit_lasanha_abobrinha.jpg",
+              "ingredients": ["Abobrinha", "Molho de Tomate", "Queijo Cottage", "Frango Desfiado"],
+              "instructions": "Corte a abobrinha em fatias e monte camadas com molho de tomate, queijo cottage e frango. Asse por 30 minutos."
+            },
+            {
+              "id": "2",
+              "name": "Pizza Integral de Frango",
+              "image_url": "https://www.example.com/images/fit_pizza_integral_frango.jpg",
+              "ingredients": ["Massa Integral", "Frango Desfiado", "Molho de Tomate", "Queijo Light"],
+              "instructions": "Espalhe o molho na massa, adicione frango desfiado e queijo. Asse a 200°C por 15 minutos."
+            },
+            {
+              "id": "3",
+              "name": "Coxinha Fit de Batata Doce",
+              "image_url": "https://www.example.com/images/fit_coxinha_batata_doce.jpg",
+              "ingredients": ["Batata Doce", "Frango Desfiado", "Farinha de Linhaça"],
+              "instructions": "Misture batata doce cozida com farinha de linhaça. Recheie com frango desfiado e asse até dourar."
+            },
+            {
+              "id": "4",
+              "name": "Bolinho de Quinoa",
+              "image_url": "https://www.example.com/images/fit_bolinho_quinoa.jpg",
+              "ingredients": ["Quinoa", "Ovos", "Cebola", "Cenoura"],
+              "instructions": "Cozinhe a quinoa e misture com ovos e vegetais. Molde bolinhos e asse a 180°C por 20 minutos."
+            },
+            {
+              "id": "5",
+              "name": "Empada de Aveia e Frango",
+              "image_url": "https://www.example.com/images/fit_empada_frango.jpg",
+              "ingredients": ["Aveia", "Frango Desfiado", "Requeijão Light"],
+              "instructions": "Prepare uma massa com aveia e recheie com frango e requeijão light. Asse por 25 minutos a 180°C."
+            },
+            {
+              "id": "6",
+              "name": "Pastel Assado Integral",
+              "image_url": "https://www.example.com/images/fit_pastel_integral.jpg",
+              "ingredients": ["Massa Integral", "Carne Moída Magra", "Cebola"],
+              "instructions": "Recheie a massa integral com carne moída. Asse a 200°C por 20 minutos."
+            },
+            {
+              "id": "7",
+              "name": "Quiche de Espinafre e Queijo Branco",
+              "image_url": "https://www.example.com/images/fit_quiche_espinafre.jpg",
+              "ingredients": ["Farinha Integral", "Espinafre", "Queijo Branco", "Ovos"],
+              "instructions": "Misture farinha com ovos e espinafre. Asse em uma forma com recheio de queijo branco."
+            },
+            {
+              "id": "8",
+              "name": "Esfiha Integral de Carne",
+              "image_url": "https://www.example.com/images/fit_esfiha_integral.jpg",
+              "ingredients": ["Massa Integral", "Carne Moída Magra", "Cebola", "Tomate"],
+              "instructions": "Recheie a massa integral com carne temperada. Asse a 180°C por 15 minutos."
+            },
+            {
+              "id": "9",
+              "name": "Torta Salgada de Legumes",
+              "image_url": "https://www.example.com/images/fit_torta_legumes.jpg",
+              "ingredients": ["Farinha Integral", "Ovos", "Cenoura", "Brócolis"],
+              "instructions": "Misture farinha com ovos e vegetais picados. Asse a 180°C por 30 minutos."
+            },
+            {
+              "id": "10",
+              "name": "Feijoada Light",
+              "image_url": "https://www.example.com/images/fit_feijoada.jpg",
+              "ingredients": ["Feijão Preto", "Peito de Frango", "Linguiça de Frango", "Couve"],
+              "instructions": "Cozinhe o feijão e adicione peito de frango e linguiça de frango. Sirva com couve refogada."
+            }
+          ]
         }
+
         """
         
         let json_data = Data(mockJson.utf8)
@@ -187,7 +209,6 @@ struct RecipeListView: View {
                     .padding(.top, 16)
                 }
             }
-            .background(Color.white)
             .onAppear {
                 viewModel.fetchRecipesFromJson()
             }
@@ -220,7 +241,9 @@ struct RecipesCarousel: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 16) {
                 ForEach(recipes.prefix(5)) { recipe in
-                    RecipeCarouselCard(recipe: recipe)
+                    NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                        RecipeCarouselCard(recipe: recipe)
+                    }
                 }
             }
             .padding(.horizontal)
@@ -262,9 +285,9 @@ struct RecipeCarouselCard: View {
 struct FullRecipeListView: View {
     let category: String
     let recipes: [RecipeModel]
-
+    
     @State private var searchText = ""
-
+    
     var filteredRecipes: [RecipeModel] {
         if searchText.isEmpty {
             return recipes
@@ -274,7 +297,7 @@ struct FullRecipeListView: View {
             }
         }
     }
-
+    
     var body: some View {
         List {
             ForEach(filteredRecipes) { recipe in
@@ -284,6 +307,7 @@ struct FullRecipeListView: View {
                 .listRowSeparator(.hidden)
             }
         }
+        .scrollContentBackground(.hidden)
         .navigationTitle("Receitas \(category)")
         .navigationBarTitleDisplayMode(.inline)
         .searchable(text: $searchText, prompt: "Buscar receitas")
@@ -311,16 +335,31 @@ struct RecipeDetailView: View {
                 Text(recipe.name)
                     .font(.largeTitle)
                     .bold()
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity)
                 
                 VStack(alignment: .leading, spacing: 10) {
                     Text("Ingredientes:")
                         .font(.headline)
-                        .padding(.top, 10)
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Capsule().fill(Color.gray.opacity(0.2)))
+                        .padding(.top, 16)
                     
                     ForEach(recipe.ingredients, id: \.self) { ingredient in
-                        Text(ingredient)
+                        Text("- \(ingredient)")
                             .font(.body)
                     }
+                    
+                    Text("Modo de preparo:")
+                        .font(.headline)
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Capsule().fill(Color.gray.opacity(0.2)))
+                        .padding(.top, 16)
+                    
+                    Text(recipe.instructions)
+                        .font(.body)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.top, 10)
@@ -332,6 +371,7 @@ struct RecipeDetailView: View {
         .scrollIndicators(.hidden)
     }
 }
+
 
 
 struct RecipeCard: View {
