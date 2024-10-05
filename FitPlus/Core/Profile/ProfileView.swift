4be07caf03df3dfa8a9fc524976e2f49552e99bf
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-05 19:04:16 -0300

    Create first layout of Profile screen

--- FitPlus/Core/Profile/ProfileView.swift
+++ FitPlus/Core/Profile/ProfileView.swift
@@ -19,34 +19,110 @@ final class ProfileViewModel: ObservableObject {
     
 }
 
+struct RoundedCorner: Shape {
+    var cornerRadius: CGFloat = 0.0
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        return Path(path.cgPath)
+    }
+}
+
 struct ProfileView: View {
     
     @StateObject private var viewModel = ProfileViewModel()
+    @State var presentSheet = false
     
     var body: some View {
         NavigationView {
+            ZStack {
+                Color.accentColor
 
-            VStack {
-                Text("Nome: \(viewModel.user?.fullName ?? "")")
-                Text("email: \(viewModel.user?.email ?? "")")
-                Text("ID: \(viewModel.user?.userId ?? "")")
-                
-                Button {
-                    do {
-                        try AuthenticationManager.shared.signOut()
-                    } catch {
-                        print("Failed to sign out")
+                VStack(spacing: 0) {
+                    Color.accentColor
+                        .frame(height: UIScreen.main.bounds.height * 0.25)
+                    
+                    VStack {
+                        
+                        Text(viewModel.user?.userName ?? "")
+                            .font(.title)
+                            .padding(.top, 64)
+                        
+                        VStack(alignment: .leading) {
+                        Text("Label 1")
+                            .font(.headline)
+                            .padding(.top, 32)
+                        
+                        Text("Nome: \(viewModel.user?.fullName ?? "")")
+                            .font(.headline)
+                        
+                        Text("email: \(viewModel.user?.email ?? "")")
+                            .font(.headline)
+
+                        Text("ID: \(viewModel.user?.userId ?? "")")
+                            .font(.headline)
+                        }
+                        
+                        VStack {
+                            Spacer()
+
+                            Button {
+                                do {
+                                    try AuthenticationManager.shared.signOut()
+                                } catch {
+                                    print("Failed to sign out")
+                                }
+                            } label: {
+                                Image(systemName: "playstation.logo")
+                                Text("Sign out")
+                            }
+                            .background(.gray.opacity(0.5))
+                            .padding(.bottom, 16)
+                            
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                     }
-                } label: {
-                    Image(systemName: "playstation.logo")
-                    Text("Sign out")
+                    .zIndex(1)
+                    .padding(8)
+                    .background(.white)
+                    .clipShape(RoundedCorner(cornerRadius: 32, corners: [.topLeft, .topRight]))
+                    .sheet(isPresented: $presentSheet) {
+                        Text("kauekaj")
+                    }
                 }
-                .padding()
+                .edgesIgnoringSafeArea(.top)
+                
+                                
+                Circle()
+                    .strokeBorder(Color.white, lineWidth: 2)
+                    .background(Circle().foregroundColor(.gray))
+                    .frame(width: 150, height: 150)
+                    .offset(y: -(UIScreen.main.bounds.height * 0.25))
+                    .onTapGesture {
+                        presentSheet.toggle()
+                    }
+                
+                Button(action: {
+                    presentSheet.toggle()
+                }) {
+                    Image(systemName: "camera")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .padding(2)
+                        .background(.cyan)
+                        .foregroundColor(.red)
+                        .offset(
+                            x: (UIScreen.main.bounds.width * 0.10),
+                            y: -(UIScreen.main.bounds.height * 0.18)
+                        )
+                        .zIndex(2)
+                }
             }
-            .task {
-                try? await viewModel.loadCurrentUser()
-//                print("kauekaj: \(String(describing: viewModel.user))")
-            }
+        }
+        .task {
+            try? await viewModel.loadCurrentUser()
         }
     }
 }
