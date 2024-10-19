53b2fd851156a4d3752890d1169af20d49318b8b
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-19 14:20:58 -0300

    Update UserManager

--- FitPlus/Firestore/UserManager.swift
+++ FitPlus/Firestore/UserManager.swift
@@ -13,32 +13,32 @@ import Foundation
 
 struct FitPlusUser: Codable {
     let userId: String
-    let fullName: String?
     let email: String?
+    let fullName: String?
     let userName: String?
     let profileImagePath: String?
     let profileImagePathUrl: String?
     
     init(auth: AuthDataResultModel) {
         self.userId = auth.uid
-        self.fullName = auth.fullName
-        self.userName = auth.userName
         self.email = auth.email
+        self.fullName = nil
+        self.userName = nil
         self.profileImagePath = nil
         self.profileImagePathUrl = nil
     }
     
     init(
         userId: String,
-        fullName: String? = nil,
         email: String? = nil,
+        fullName: String? = nil,
         userName: String? = nil,
         profileImagePath: String? = nil,
         profileImagePathUrl: String?  = nil
     ) {
         self.userId = userId
-        self.fullName = fullName
         self.email = email
+        self.fullName = fullName
         self.userName = userName
         self.profileImagePath = profileImagePath
         self.profileImagePathUrl = profileImagePathUrl
@@ -46,8 +46,8 @@ struct FitPlusUser: Codable {
     
     enum CodingKeys: String, CodingKey {
         case userId = "user_id"
-        case fullName = "full_name"
         case email = "email"
+        case fullName = "full_name"
         case userName = "user_name"
         case profileImagePath = "profile_image_path"
         case profileImagePathUrl = "profile_image_path_url"
@@ -56,8 +56,8 @@ struct FitPlusUser: Codable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.userId = try container.decode(String.self, forKey: .userId)
-        self.fullName = try container.decodeIfPresent(String.self, forKey: .fullName)
         self.email = try container.decodeIfPresent(String.self, forKey: .email)
+        self.fullName = try container.decodeIfPresent(String.self, forKey: .fullName)
         self.userName = try container.decodeIfPresent(String.self, forKey: .userName)
         self.profileImagePath = try container.decodeIfPresent(String.self, forKey: .profileImagePath)
         self.profileImagePathUrl = try container.decodeIfPresent(String.self, forKey: .profileImagePathUrl)
@@ -66,8 +66,8 @@ struct FitPlusUser: Codable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(self.userId, forKey: .userId)
-        try container.encodeIfPresent(self.fullName, forKey: .fullName)
         try container.encodeIfPresent(self.email, forKey: .email)
+        try container.encodeIfPresent(self.fullName, forKey: .fullName)
         try container.encodeIfPresent(self.userName, forKey: .userName)
         try container.encodeIfPresent(self.profileImagePath, forKey: .profileImagePath)
         try container.encodeIfPresent(self.profileImagePathUrl, forKey: .profileImagePathUrl)
