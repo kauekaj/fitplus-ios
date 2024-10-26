24323b42187a236ba597235eb5d45a9b7da808cd
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-26 20:58:54 -0300

    Create StorageManager

---
+++ FitPlus/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIImage+Extensions.swift
+//  FitPlus
+//
+//  Created by Kaue de Assis Jacyntho on 26/10/24.
+//
+
+import SwiftUI
+
+extension UIImage {
+    static func fromAsset(named name: String) -> UIImage? {
+        return UIImage(named: name)
+    }
+}
