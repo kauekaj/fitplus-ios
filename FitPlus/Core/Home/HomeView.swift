f1f8f6b68f7e6f475eea2bf3c08831127498df5d
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-28 21:47:29 -0300

    Refactor HomeView Content

--- FitPlus/Core/Home/HomeView.swift
+++ FitPlus/Core/Home/HomeView.swift
@@ -8,24 +8,137 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var userRepository: UserRepository
+    
+    private var user: FitPlusUser? {
+        userRepository.getUser()
+    }
+    
+    private var firstName: String {
+        user?.fullName?.split(separator: " ").first.map(String.init) ?? ""
+    }
+    
+    var favoriteRecipes: [String] = []
+    
     var body: some View {
-        ZStack {
-            
-            Button {
-                do {
-                    try AuthenticationManager.shared.signOut()
-                } catch {
-                    print("Failed to sign out")
-                }
-            } label: {
-                Image(systemName: "playstation.logo")
-                Text("Sign out")
-            }
+        NavigationStack {
+            ScrollView {
+                VStack() {
+                    
+                    HStack {
+                        Text("Olá, \(firstName) 👋")
+                            .font(.title2)
+                            .bold()
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // To implement
+                        }) {
+                            Image(systemName: "bell.fill")
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding(4)
+                                .background(.gray.opacity(0.2))
+                                .clipShape(Circle())
+                        }
+                    }
+                    .padding()
+                    
+                    let homeCards = ["Mitos e verdades", "Listas", "???"]
 
+                    HStack {
+                        ForEach(Array(homeCards.enumerated()), id: \.element) { index, card in
+                            NavigationLink(destination: Text("Tela \(card)")) {
+                                Color.accentColor
+                                    .frame(width: 100, height: 100)
+                                    .cornerRadius(8)
+                                    .overlay(
+                                        Text("\(card)")
+                                            .foregroundColor(.white)
+                                            .font(.headline)
+                                    )
+                            }
+                            
+                            if index < homeCards.count - 1 {
+                                Spacer()
+                            }
+                        }
+                    }
+                    .padding(24)
+                    
+                    ZStack(alignment: .bottom) {
+                        Image("dailyMessageCover")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width - 16, height: 200)
+
+                            .shadow(radius: 4)
+                            .clipped()
+                            .cornerRadius(16)
+                        
+                        Text("Já leu a mensagem do dia hoje?")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding(8)
+                            .background(Color.black.opacity(0.6))
+                            .cornerRadius(8)
+                            .padding(.bottom, 4)
+                            .padding(.horizontal, 8)
+                    }
+                    .padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Receitas Favoritas")
+                            .font(.headline)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                        
+                        if favoriteRecipes.isEmpty {
+                            NavigationLink(destination: RecipeListView()) {
+                                ZStack(alignment: .bottom) {
+                                    Image("emptyRecipe")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(height: 150)
+                                        .clipped()
+                                        .cornerRadius(16)
+                                    
+                                    Text("Conheça nossas receitas")
+                                        .foregroundColor(.white)
+                                        .font(.headline)
+                                        .padding(8)
+                                        .background(Color.black.opacity(0.6))
+                                        .cornerRadius(8)
+                                        .padding(.bottom, 4)
+                                        .padding(.horizontal, 8)
+                                }
+                                .padding(.horizontal)
+                            }
+
+
+                        } else {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 16) {
+                                    ForEach(favoriteRecipes, id: \.self) { recipe in
+                                        Color.accentColor
+                                            .frame(width: 120, height: 120)
+                                            .cornerRadius(12)
+                                            .overlay(
+                                                Text(recipe)
+                                                    .foregroundColor(.white)
+                                                    .font(.headline)
+                                            )
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                    }
+                    .padding(8)
+                }
+            }
         }
     }
-}
-
-#Preview {
-    HomeView()
 }
---
+++ FitPlus/Core/MythOrTruth/MythOrTruthView.swift
@@ -0,0 +1,18 @@
+//
+//  MythOrTruthView.swift
+//  FitPlus
+//
+//  Created by Kaue de Assis Jacyntho on 28/10/24.
+//
+
+import SwiftUI
+
+struct MythOrTruthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MythOrTruthView()
+}
