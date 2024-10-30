540717f51f5fa0bcddddb115c69e3bc50da58df5
Kaue Jacyntho <kauekaj@gmail.com>
2024-10-30 19:15:18 -0300

    Add intermediate loading animation at ContentView

--- FitPlus/Root/View/ContentView.swift
+++ FitPlus/Root/View/ContentView.swift
@@ -10,17 +10,31 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel = ContentViewModel()
+    @State private var showIntermediateLoading = false
     
     var body: some View {
         Group {
-            if viewModel.isLoading {
+            if viewModel.isLoading || showIntermediateLoading {
                 ProgressView()
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 0.3), value: showIntermediateLoading)
             } else {
                 if viewModel.userSession != nil {
                     TabbarView()
+                        .transition(.move(edge: .trailing))
+                        .animation(.easeInOut(duration: 0.5), value: viewModel.userSession)
                 } else {
                     LoginView()
+                        .transition(.move(edge: .leading))
+                        .animation(.easeInOut(duration: 0.5), value: viewModel.userSession)
                 }
+            }
+        }
+        .onChange(of: viewModel.userSession) { newSession in
+            showIntermediateLoading = true
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                showIntermediateLoading = false
             }
         }
     }
